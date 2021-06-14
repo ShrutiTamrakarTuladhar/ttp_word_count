35928fef51e3c776931d2fefb7ec84d39d6b12e8
Shruti Tamrakar Tuladhar <bevy@Shrutis-MacBook-Pro.local>
2021-06-13 23:26:26 -0400

    wordcount done

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState } from "react";
 
 function App() {
+  const [text, setText] = useState("");
+  const [charCount, setCharCount] = useState(0);
+  const [wordCount, setWordCount] = useState(0);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(text);
+    console.log(text.length);
+  };
+
+  const handleChange = (e) => {
+    setText(e.target.value);
+    setCharCount(e.target.value.length);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Text
+          <textarea type="text" value={text} onChange={handleChange}></textarea>
+        </label>
+        <button type="submit" value="Submit">
+          submit
+        </button>
+      </form>
+      <p>Current chars : {charCount}</p>
     </div>
   );
 }
